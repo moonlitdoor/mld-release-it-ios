ae2ec2bbe0b54ba5495cbeb4350b76629abea6ce
Brian Amesbury <bamesbury@sofi.org>
2018-12-19 19:53:21 -0700

    Travis-CI 2

--- release-it/ViewController.swift
+++ release-it/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        RecordAudio(Label)
+    }
+    
+    @IBAction func RecordAudio(_ sender: Any) {
+        Label.text = "Help!!!"
+    }
+    
+    @IBAction func erts(_ sender: UIButton) {
+    }
 }
 
